e5dc2a1636e63a5d52ac7a1c38c1f5dde56ce072
iain <iainfraser188@msn.com>
2021-10-23 01:50:02 +0100

    first extention

--- codeclan_karaoke/classes/room.py
+++ codeclan_karaoke/classes/room.py
@@ -1,17 +1,31 @@
 class Room:
-    def __init__(self,room_number):
+    def __init__(self,room_number,room_capacity):
         self.room_number = room_number
         self.guests_in_room = []
+        self.playlist = []
+        self.room_capacity = room_capacity
 
 
 
     def check_guest_into_room(self, name):
-        self.guests_in_room.append(name)
-
+       if len(self.guests_in_room) < self.room_capacity :
+          self.guests_in_room.append(name)
+    #    else:
+    #       room_2.check_guest_into_room(name) 
+       
     def check_guest_out_of_room(self,name):
         for guest in self.guests_in_room:
             if guest == name :
                 self.guests_in_room.remove(name)   
+
+    def add_song_to_playlist(self,song):
+        self.playlist.append(song) 
+
+    # def overcrowded(self):
+    #     if len(self.guests_in_room) == 2:
+    #         return True
+
+            
 
           
             
--- codeclan_karaoke/tests/room_test.py
+++ codeclan_karaoke/tests/room_test.py
@@ -5,9 +5,13 @@ from classes.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.room_1 = Room(1)
+        self.room_1 = Room(1,2)
+        self.room_2 = Room(2,5)
         self.guest_1 = Guest("iain")
         self.guest_2 = Guest("steve")
+        self.guest_3 = Guest("peter")
+        self.guest_4 = Guest("dave")
+        self.song_1 = Song("another one bytes the dust")
 
     
     def test_check_room(self):
@@ -23,6 +27,17 @@ class TestRoom(unittest.TestCase):
         self.room_1.check_guest_out_of_room(self.guest_2)
         self.assertEqual(1,len(self.room_1.guests_in_room))
 
-        
+    def test_add_song_to_playlist(self):
+        self.room_1.add_song_to_playlist(self.song_1)
+        self.assertEqual("another one bytes the dust",self.room_1.playlist[0].song_name)  
+
+    def test_over_crowded(self):
+        self.room_1.check_guest_into_room(self.guest_1)  
+        self.room_1.check_guest_into_room(self.guest_2) 
+        self.room_1.check_guest_into_room(self.guest_3)
+        self.assertEqual(2,len(self.room_1.guests_in_room))
+        # self.assertEqual(1,len(self.room_2.guests_in_room))
+
+            
 
 
